75e09abf7ff5873a09aed2674fb9293fd67d9ba3
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-03-23 06:26:59 +0600

    testimonials sec design created

--- src/Components/Home/Testimonials.jsx
+++ src/Components/Home/Testimonials.jsx
@@ -1,9 +1,46 @@
+import Title from "../Title";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation, Autoplay } from "swiper/modules";
 
 const Testimonials = () => {
+
+  
+
   return (
-    <div>
-      Testimonials
+    <div className="my-10">
+      <Title
+        title="Testimonials"
+        subTitle="Stories of Success and Satisfaction"
+        desc="Testimonials are brief statements that express satisfaction or approval, often used in marketing to build trust and credibility."
+      />
+
+      <div className="my-10">
+        <Swiper
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
