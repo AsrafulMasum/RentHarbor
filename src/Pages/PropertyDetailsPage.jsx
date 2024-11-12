b7aba32729700ed675245d94ee6e0272458187cd
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-11-13 04:06:27 +0600

    problem solved that occurs on seeing image in property details page

--- src/Pages/PropertyDetailsPage.jsx
+++ src/Pages/PropertyDetailsPage.jsx
@@ -1,56 +1,41 @@
 import { useParams } from "react-router-dom";
 import useLoadPublicData from "../Hooks/useLoadPublicData";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 
 function PropertyDetailsPage() {
   const { id } = useParams();
 
   const { data } = useLoadPublicData(`properties/${id}`);
   const property = data?.property;
-  console.log(typeof (property?.images));
 
   return (
     <div className="max-w-screen-xl mx-4 lg:mx-auto mt-28">
-      {/* <Swiper
-        style={{
-          "--swiper-pagination-color": "#FD6C23",
-        }}
-        slidesPerView={1}
-        spaceBetween={30}
-        modules={[Pagination]}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-          1280: {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        {property?.images?.map((img, idx) => (
-          <SwiperSlide key={idx} className="overflow-hidden pb-10">
-            <img
-              className="w-4/6 h-80 mx-auto"
-              src={img}
-              alt="propertyImg"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
+      {property && (
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "#FD6C23",
+          }}
+          slidesPerView={'auto'}
+          spaceBetween={30}
+          loop={true}
+          modules={[Navigation]}
+          navigation={{
+            clickable: true,
+          }}
+          className="md:w-4/6 mx-auto rounded-lg"
+        >
+          {property?.images?.map((img, idx) => (
+            <SwiperSlide key={idx} className="overflow-hidden pb-10">
+              <img className="w-full h-96 object-cover rounded-lg" src={img} alt="propertyImg" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   );
 }
