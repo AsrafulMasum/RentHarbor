c43d878870c966265909653e8a2d0b76d3c8fac2
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2026-02-28 18:52:23 +0600

    almighty push

--- src/Components/Dashboard/HostDashboard.jsx
+++ src/Components/Dashboard/HostDashboard.jsx
@@ -5,59 +5,59 @@ import { IoHomeOutline } from "react-icons/io5";
 import { IoIosTimer } from "react-icons/io";
 import RentBarChart from "../charts/RentBarChart";
 import EarningLineChart from "../charts/EarningLineChart";
-
+import { FcLike } from "react-icons/fc";
 
 const HostDashboard = () => {
-    const { user } = useContext(AuthContext);
-    console.log(user);
-  
-    const statistics = [
-      {
-        title: "Total Earnings",
-        amount: "28K",
-        icon: <BsGraphUpArrow className="text-3xl text-primary" />,
-      },
-      {
-        title: "Total Properties",
-        amount: "18",
-        icon: <IoHomeOutline className="text-3xl text-primary" />,
-      },
-      {
-        title: "Host from",
-        amount: "20 days",
-        icon: <IoIosTimer className="text-4xl text-primary" />,
-      },
-      {
-          title: "Total Reviews",
-          amount: "20 days",
-          icon: <IoIosTimer className="text-4xl text-primary" />,
-      },
-    ];
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  const statistics = [
+    {
+      title: "Total Earnings",
+      amount: "28K",
+      icon: <BsGraphUpArrow className="text-3xl text-primary" />,
+    },
+    {
+      title: "Total Properties",
+      amount: "18",
+      icon: <IoHomeOutline className="text-3xl text-primary" />,
+    },
+    {
+      title: "Total Reviews",
+      amount: "20",
+      icon: <FcLike className="text-4xl text-primary" />,
+    },
+    {
+      title: "Host from",
+      amount: "20 days",
+      icon: <IoIosTimer className="text-4xl text-primary" />,
+    },
+  ];
 
   return (
     <div className="w-full py-10 px-10 max-h-screen overflow-y-auto">
-        <div className="grid grid-cols-3 gap-6 h-[120px] mb-4">
-          {statistics?.map((statistic) => (
-            <div
-              key={statistic?.title}
-              className="bg-white border rounded-lg p-[25px] flex items-center gap-8 shadow-md"
-            >
-              <div>{statistic?.icon}</div>
-              <div className="flex flex-col gap-1">
-                <h2 className="text-lg font-medium text-sub_title">
-                  {statistic?.title}
-                </h2>
-                <h3 className="text-sub_title text-2xl font-semibold">
-                  {statistic?.amount}
-                </h3>
-              </div>
+      <div className="grid grid-cols-3 gap-6 h-[120px] mb-4">
+        {statistics?.map((statistic) => (
+          <div
+            key={statistic?.title}
+            className="bg-white border rounded-lg p-[25px] flex items-center gap-8 shadow-md"
+          >
+            <div>{statistic?.icon}</div>
+            <div className="flex flex-col gap-1">
+              <h2 className="text-lg font-medium text-sub_title">
+                {statistic?.title}
+              </h2>
+              <h3 className="text-sub_title text-2xl font-semibold">
+                {statistic?.amount}
+              </h3>
             </div>
-          ))}
-        </div>
-        <RentBarChart />
-        <EarningLineChart />
+          </div>
+        ))}
       </div>
-  )
-}
+      <RentBarChart />
+      <EarningLineChart />
+    </div>
+  );
+};
 
-export default HostDashboard
+export default HostDashboard;
